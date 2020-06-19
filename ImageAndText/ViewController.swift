bc4ad83dfe2e0064e99be0cb08319dd28fec86ee
Furkan <furkancetinalpemin@gmail.com>
2020-06-19 09:58:28 +0300

    Initial Commit

---
+++ ImageAndText/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImageAndText
+//
+//  Created by Furkan Emin Çetinalp on 19.06.2020.
+//  Copyright © 2020 x-swift. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
